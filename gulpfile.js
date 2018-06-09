58d199d0621fd58846fa17d627be2b719ff11ec6
Nikolay <superturb0@yandex.ru>
2018-06-09 23:27:29 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,47 +8,55 @@ var uncss        = require('gulp-uncss');
 // Статический сервер
     gulp.task('serve', ['sass'], function(){
         browserSync.init({
-            server: './'
+            server: './dist'
         });
-
-        gulp.watch('scss/*.scss', ['sass']);
-        gulp.watch('*.html').on('change', browserSync.reload);
     });
 
 // Scss в css
     gulp.task('sass', function(){
-        gulp.src('scss/*.scss')
+        gulp.src('src/scss/*.scss')
             .pipe(sass().on('error', sass.logError))
-            .pipe(gulp.dest('css'))
-            .on('error', sass.logError)
-            .pipe(browserSync.stream());
-    });
-
-// Автопрефикс
-    gulp.task('autoprefixer', function (){
-        gulp.src('css/*.css')
             .pipe(autoprefixer({
                 browsers: ['last 2 versions'],
                 cascade: false
             }))
-            .pipe(gulp.dest('css'));
+            .pipe(gulp.dest('dist/css'))
+            .on('error', sass.logError)
+            .pipe(browserSync.reload({
+                stream: true
+            }));
+    });
+
+// Автопрефикс
+    gulp.task('autoprefixer', function (){
+        gulp.src('dist/css/*.css')
+            .pipe(autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            }))
+            .pipe(gulp.dest('dist/css'));
     });
 
 // Минификация css
     gulp.task('minify-css', function(){
-        gulp.src('css/*.css')
+        gulp.src('dist/css/*.css')
             .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(gulp.dest('css'));
+            .pipe(gulp.dest('dist/css'));
 });
 
 // Оичстка неиспользуемых стилий
     gulp.task('uncss', function(){
-        gulp.src('css/*.css')
+        gulp.src('dist/css/*.css')
             .pipe(uncss({
-                html: ['*.html']
+                html: ['dist/*.html']
             }))
-            .pipe(gulp.dest('css'));
+            .pipe(gulp.dest('dist/css'));
     });
 
+    gulp.task('watch', function(){
+        gulp.watch('src/scss/*.scss', ['sass']);
+        gulp.watch('dist/*.html').on('change', browserSync.reload);
+    })
+
 // Запуск browserSync и компиляция scss в css
-    gulp.task('default', ['serve']);
+    gulp.task('default', ['serve','watch','sass']);
